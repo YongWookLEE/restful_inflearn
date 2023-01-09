4e84e80ba067c2dd53656faebcd19f92d90113c9
YongWookLee <lsd1008@naver.com>
2023-01-09 22:19:47 +0900

    230109

--- restful-web-service/src/main/java/com/example/restfulwebservice/user/UserV2.java
+++ restful-web-service/src/main/java/com/example/restfulwebservice/user/UserV2.java
@@ -1,8 +1,6 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -21,17 +19,8 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@JsonIgnoreProperties(value = {"ssn"})
-@JsonFilter("UserInfo")
-public class User {
-    private Integer id;
-    @Size(min=2, message = "Name은 2글자 이상 입력해 주세요.")
-    private String name; 
-    @Past
-    private Date joinDate;
-//    @JsonIgnore
-    private String password;
-//    @JsonIgnore
-    private String ssn;
+@JsonFilter("UserInfoV2")
+public class UserV2 extends User {
+    private String grade;
 }
                                     
